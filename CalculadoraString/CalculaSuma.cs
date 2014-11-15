38ce1fe38b1932359f55526a0f23de07596b3e3b
Alexander Raúl Cabrera Guevara <arcabrerag@outlook.com>
2014-11-15 10:34:38 -0500

    CalculadoraStringTDD.Vs.1.0
    Calculadora string funcional completado en tiempos de ocio. :100:

---
+++ CalculadoraString/CalculaSuma.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace CalculadoraString
+{
+
+    public class CalculaSuma
+    {
+
+        public string CalculadoraString(string CadenaNumeros)
+        {
+
+            if (CadenaNumeros == "")
+            {
+                return "0";
+            }
+
+            else
+            {
+
+                List<int> list = new List<int>();
+
+                Regex delimitador = new Regex(",");
+                string[] subNumero = delimitador.Split(CadenaNumeros);
+                foreach (string Numerito in subNumero)
+                {
+                    if (Convert.ToInt32(Numerito) >= 0 && Convert.ToInt32(Numerito) < 1000)
+                    {
+                        list.Add(Convert.ToInt32(Numerito));
+                    }
+
+                    if (Convert.ToInt32(Numerito) < 0)
+                    {
+                        throw new InvalidDataException();
+                    }
+                }
+
+                return Convert.ToString(list.Sum());
+            }
+
+        }
+    }
+}
---
+++ TestString/SumaStringTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using NUnit.Framework;
+using CalculadoraString;
+
+namespace TestString
+{
+    public class SumaStringTest
+    {
+
+        private CalculaSuma calculaSuma { get; set; }
+
+        [SetUp]
+        public void InstaciasAntesDeCadaTest()
+        {
+            // Arrange
+            calculaSuma = new CalculaSuma();
+        }
+
+        [Test]
+        public void TestStringSuma15y15y5y10()
+        {
+            // Act
+            var result = calculaSuma.CalculadoraString("15,15,5,10");
+
+            // Assert
+            Assert.AreEqual("45", result);
+        }
+
+        [Test]
+        public void TestStringSumaNada()
+        {
+            // Act
+            var result = calculaSuma.CalculadoraString("");
+
+            // Assert
+            Assert.AreEqual("0", result);
+        }
+
+        [Test]
+        public void TestStringSuma15y15y1000y5y10()
+        {
+            // Act
+            var result = calculaSuma.CalculadoraString("15,15,1000,5,10");
+
+            // Assert
+            Assert.AreEqual("45", result);
+        }
+
+        [Test]
+        public void TestStringSuma15y15yMenos3y5y10()
+        {
+            // Act
+            var result = calculaSuma.CalculadoraString("15,15,-3,5,10");
+
+            // Assert
+            Assert.AreEqual(new InvalidDataException(), result);
+        }
+
+        [Test]
+        public void TestStringSuma15y15y5000y5y10()
+        {
+            // Act
+            var result = calculaSuma.CalculadoraString("15,15,5000,5,10");
+
+            // Assert
+            Assert.AreEqual("45", result);
+        }
+    }
+}
